23f4351081c9fac972da8442cdacb9ff8e584a94
dinaahmed10 <codedinaaa@gmail.com>
2025-06-17 06:01:53 +0300

    V1

--- src/main/java/com/spring/restaurant/Entity/User.java
+++ src/main/java/com/spring/restaurant/Entity/User.java
@@ -2,10 +2,11 @@ package com.spring.restaurant.Entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @Setter
 @Getter
@@ -18,9 +19,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @NotNull(message = "firstName is required")
+    @NotEmpty(message = "firstName cannot be empty")
+    @NotBlank(message = "firstName cannot be blank")
     private String firstName;
     @Column(nullable = false)
+    @NotNull(message = "lastName is required")
+    @NotEmpty(message = "lastName cannot be empty")
+    @NotBlank(message = "lastName cannot be blank")
     private String lastName;
     @Column(nullable = false,unique = true)
+    @Email(message = "Invalid email address")
     private String email;
 }
